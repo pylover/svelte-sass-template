dc98ee10562e0759a820da6025be0199bf8d671f
pylover <vahid.mardani@gmail.com>
2021-07-09 17:10:32 +0430

    Use yhttp ubstead of sirv

--- server.py
+++ server.py
@@ -1,15 +1,8 @@
 #! /usr/bin/env python3
 import sys
-import signal
 
 from yhttp import Application, text
 
-
-def sighandler(sig, frame):
-    sys.exit(0)
-
-
-signal.signal(signal.SIGTERM, sighandler)
 
 app = Application()
 app.staticdirectory('/', 'public', default=True, fallback=True)
@@ -17,4 +10,5 @@ app.ready()
 try:
     app.climain(['s'])
 except OSError:
+    print('The port is alredy taken.')
     pass
